0b877b505381ec5ae89dbb23a58d2cb0546afc4c
Harry Finn <harry.finn@1minus1.com>
2015-10-26 10:38:05 +0000

    Add option value to store current job feed departments
    * Allows for departments to be return to frontend for use in filters for
    each job post

--- admin/class.jobviteapi.php
+++ admin/class.jobviteapi.php
@@ -1,20 +1,28 @@
 <?php
+
 class JobviteAPI {
   public $api_url,
          $api_key,
          $api_secret,
          $api_companyId;
+
   public function __construct($api_key, $api_secret, $api_companyId) {
     $this->api_url = 'https://api.jobvite.com/v1/jobFeed';
     $this->api_key = $api_key;
     $this->api_secret = $api_secret;
     $this->api_companyId = $api_companyId;
   }
-  public function get_results($fetch_type = 'External') {
+
+  public function get_jobs($fetch_type = 'External') {
     return $this->map_feed_fields(
       json_decode($this->get_api_response($fetch_type))
     );
   }
+
+  public function get_departments($jobs) {
+    return $this->fetch_job_departments($jobs);
+  }
+
   private function build_api_url($type) {
     return $this->api_url .
            '?api=' . $this->api_key .
@@ -22,6 +30,7 @@ class JobviteAPI {
            '&companyId=' . $this->api_companyId .
            '&availableTo=' . $type;
   }
+
   private function get_api_response($type) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -29,8 +38,10 @@ class JobviteAPI {
     curl_setopt($curl, CURLOPT_URL, $this->build_api_url($type));
     $result = curl_exec($curl);
     curl_close($curl);
+
     return $result;
   }
+
   private function map_feed_fields($feed) {
     $fields_to_map = [
       'title' => 'title',
@@ -41,8 +52,10 @@ class JobviteAPI {
       'description' => 'description',
       'department' => 'department'
     ];
+
     foreach($feed->jobs as $job) {
       $job_array = (array) $job;
+
       $filter_field_keys = array_intersect_key(
         array_flip($fields_to_map),
         $job_array
@@ -50,8 +63,24 @@ class JobviteAPI {
       $remap_fields = array_intersect_key($job_array, $filter_field_keys);
       ksort($filter_field_keys);
       ksort($remap_fields);
+
       $job_feed[$job_array['id']] = array_combine($filter_field_keys, $remap_fields);
     }
+
     return $job_feed;
+  }
+
+  private function fetch_job_departments($jobs) {
+    $departments = [];
+
+    foreach($jobs as $job) {
+      $department = $job['department'];
+
+      if(!in_array($department, $departments)) {
+        $departments[] = $department;
+      }
+    }
+
+    return $departments;
   }
 }
